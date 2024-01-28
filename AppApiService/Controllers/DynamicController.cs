138481d4668c7198ed38b72f94efdf0b47ffeefc
XiaotongZhao <275451183@qq.com>
2024-01-28 14:27:58 +0800

    添加动态数据处理代码

--- AppApiService/Controllers/DynamicController.cs
+++ AppApiService/Controllers/DynamicController.cs
@@ -1,6 +1,8 @@
 ﻿using AppApiService.Domain.DynamicRequestDataService;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
 
 namespace AppApiService.Controllers
 {
@@ -37,5 +39,35 @@ namespace AppApiService.Controllers
             var res = await dynamicDataService.GetDataMapList();
             return res;
         }
+
+        [HttpPost, Route("TestDynamicData")]
+        public void TestDynamicData(dynamic data)
+        {
+            var jsonObject = JObject.Parse(data.ToString());
+            foreach (var property in jsonObject.Properties())
+            {
+                var propertyName = property.Name;
+                JToken propertyValue = property.Value;
+                var propertyType = propertyValue.Type;
+                if (propertyType == JTokenType.String)
+                {
+                    Console.WriteLine($"{propertyName} is a string");
+                }
+                else if (propertyType == JTokenType.Integer)
+                {
+                    Console.WriteLine($"{propertyName} is an integer");
+                }
+                else if (propertyType == JTokenType.Boolean)
+                {
+                    Console.WriteLine($"{propertyName} is a boolean");
+                }
+                else if (propertyType == JTokenType.Object)
+                {
+                    Console.WriteLine($"{propertyName} is a Object");
+                }
+                Console.WriteLine($"{property.Name}: {property.Value}");
+            }
+        }
+
     }
 }
