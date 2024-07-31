5e57c2d74818f39178afb1aa86ec17f0e8f4af12
XiaotongZhao <275451183@qq.com>
2024-07-31 22:29:58 +0800

    commit change code

--- AppApiService.Infrastructure.Common/SkipAndCount.cs
+++ AppApiService.Infrastructure.Common/SkipAndCount.cs
@@ -7,7 +7,7 @@ public static class SkipAndCount
         DataSource<TSource> result = new DataSource<TSource>();
         result.Data = source.Skip(skip).Take(size).ToList();
         result.Count = source.Count();
-        return result; ;
+        return result;
     }
 
     public static async Task<DataSource<TSource>> TakePageDataAndCountAsync<TSource>(this IQueryable<TSource> source, int skip, int size)
@@ -15,6 +15,6 @@ public static class SkipAndCount
         DataSource<TSource> result = new DataSource<TSource>();
         result.Data = await source.Skip(skip).Take(size).ToListAsync();
         result.Count = await source.CountAsync();
-        return result; ;
+        return result;
     }
 }
