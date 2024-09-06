bfe606f840a8a3758109d2f53a4f035ad352403f
赵筱彤 <xzhao@mycwt.com>
2024-09-06 18:16:19 +0800

    add ServiceTask

---
+++ AppApiService.Domain/DevOps/ServiceTask/ServiceTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppApiService.Domain.DevOps.ServiceTask;
+
+public class ServiceTask : EntityBase<int>
+{
+    public string TaskName { get; set; }
+    public string TaskDescription { get; set; }
+    public int StepNo { get; set; }
+    public string Log { get; set; }
+}
